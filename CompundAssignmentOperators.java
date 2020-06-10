794eac0b060e4f36f19aa91bd1421492aada8791
lauraNC <lkmaam@gmail.com>
2020-06-10 19:50:50 +0100

    Compound Operators initial commit

---
+++ CompundAssignmentOperators.java
@@ -0,0 +1,16 @@
+public class CompundAssignmentOperators {
+
+    public static void main (String[] args){
+
+        //compound - doing 2 things
+
+        int x = 2;
+        int y= 3 ;
+
+        x = x*2;
+        x *=y; // shorter form of x = x *z
+
+
+        System.out.println("x= "+ x);
+    }
+}
